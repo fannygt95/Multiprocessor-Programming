32db0ebaca587cd97472c7181166e6c2878a7e52
fannygt95 <32515988+fannygt95@users.noreply.github.com>
2019-03-17 13:49:09 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -71,50 +71,56 @@ void reduceTheMatrix(vector<unsigned char> imagen, unsigned char reducematrix[73
 
 
 void project(unsigned char im0[735][504], unsigned char im1[735][504]){
-	double average0, average1;
-	for (unsigned i = windowSize / 2; i < height - windowSize / 2; i++){
+	int count0 = 0, count1 = 0;  //Contador para la suma de los datos de las ventanas
+	int vector0[pow(windowSize)] = null; //Guarda los datos de la ventana de la primera imagen
+	int vector1[pow(windowSize)] = null; //Guarda los datos de la ventana de la segunda imagen
+	unsigned average0, average1; //Calcular medias
+	double desTipica0 = 0, desTipica1 = 0; //Calcular desviaciones típicas
+	double covarianza = 0; // Calcular covarianza
+
+	for (unsigned i = windowSize / 2; i < height - windowSize / 2; i++){  //RECORRER IMAGEN
 		for (int j = windowSize / 2; j < width - windowSize / 2; j++){
 			
-			for (int d = 0; d < disparity; d++){
-				int count0 = 0, count1 = 0;
-				
-				for (int win_y = j - windowSize / 2; win_y < windowSize; win_y++){
+			int i = 0;
+			for (int win_y = j - windowSize / 2; win_y < windowSize; win_y++) {// RECORRER VENTANA IMAGEN 0
+				for (int win_x = i - windowSize / 2; win_x < windowSize; win_x++) {
+					
+						count0 = count0 + im0[win_y][win_x]; //Sumatorio datos ventana imagen 0
+						vector0[i] = im0[win_y][win_x]; //Guardar datos
+						i++;
+				}
+			}
+			average0 = count0 / pow(windowSize); //Media ventana imagen0
+			
+			for(i = 0; i < vector0.size; i++){//RECORRER OTRA VEZ VENTANA IMAGEN 0, esta vez usamos el vector
+				desTipica0 = desTipica0 + pow(vector0[i]-average0);
+			}
+			desTipica0 = sqrt(desTipica0 / pow(windowSize));
+
+			for (int d = 0; d < disparity; d++){ //RECORRER VENTANAS IMAGEN 1
+
+				int j = 0;
+				for (int win_y = j - windowSize / 2; win_y < windowSize; win_y++){ //CONTADOR DE LOS DATOS VENTANA IMAGEN 1
 					for (int win_x = i - windowSize / 2; win_x < windowSize; win_x++){
-						count0 = count0 + im0[win_y][win_x]; //SUMA DE TODAS LAS VENTANAS
 						count1 = count1 + im1[win_y][win_x];
+						vector[j] = im1[win_y][win_x]; //CREAR VECTOR DE DATOS VENTANA IMAGEN 1
+						j++;
 					}
 				}
-				average0 = count0 / (windowSize * windowSize); //MEDIA DE LAS VENTANAS
-				average1 = count1 / (windowSize * windowSize);
-
-				double covarianza = 0;
-				double desTipica0 = 0, desTipica1 = 0;
+				average1 = count1 / pow(windowSize);
 				
-				
-				for (int win_y = j - windowSize / 2; win_y < windowSize; win_y++){
-					for (int win_x = i - windowSize / 2; win_x < windowSize; win_x++){
-				//////////////////////////////////  ZNCC VALUE  ////////////////////////////////////////
-						double aux0 = im0[win_y][win_x];
-						double aux1 = im1[win_y][win_x];
-						// covarianza//
-						covarianza = covarianza + ((aux0 - average0) * (aux1 - average1));
-						
-						// desviacion tipica imagen 0//						
-						aux0 = aux0 - average0;
-						desTipica0 = desTipica0 + pow(aux0, 2);						
-						
-						// desviacion tipica imagen 1//						
-						aux1 = aux1 - average1;
-						desTipica1 = desTipica1 + pow(aux1, 2);
-						
-
-					}
+				for(J = 0; J < vector0.size; J++){//RECORRER OTRA VEZ VENTANA IMAGEN 1, esta vez usamos el vector
+					desTipica1 = desTipica1 + pow(vector1[J]-average0);
 				}
-				desTipica0 = sqrt(desTipica0 / (windowSize * windowSize));
-				desTipica1 = sqrt(desTipica1 / (windowSize * windowSize));
+				desTipica1 = sqrt(desTipica1 / pow(windowSize));
 
-				covarianza = covarianza / (windowSize * windowSize);
-				correlation = covarianza / (desTipica0 * desTipica1);
+				for (j = 0; j < vector0.size; j++) { //CALCULAR COVARIANZA CON LOS DATOS GUARDADOS EN LOS VECTORES
+					covarianza = covarianza + ((vector0[j] - average0) * (vector1[j] - average1));
+				}
+				covarianza = covarianza / pow(windowSize);
+
+				correlation = covarianza / (desTipica0 * desTipica1); // CORRELACIÓN
+				
 				// WE ARE GOING TO COMPARING ONE WINDOW IN THE IMG0 WITH 260 WINDOWS IN THE IMG1 AND WE TAKE THE BIGGEST ONE 
 				// WHICH IT IS THE BIGGEST DISPARITY
 				if (correlation > biggestCorrelation){ 
