038e774c288b72735a2395ff39bdc0983b35bc6d
fannygt95 <32515988+fannygt95@users.noreply.github.com>
2019-03-17 14:11:57 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -81,41 +81,41 @@ void project(unsigned char im0[735][504], unsigned char im1[735][504]){
 	for (unsigned i = windowSize / 2; i < height - windowSize / 2; i++){  //RECORRER IMAGEN
 		for (int j = windowSize / 2; j < width - windowSize / 2; j++){
 			
-			int i = 0;
+			int m = 0;
 			for (int win_y = j - windowSize / 2; win_y < windowSize; win_y++) {// RECORRER VENTANA IMAGEN 0
 				for (int win_x = i - windowSize / 2; win_x < windowSize; win_x++) {
 					
 						count0 = count0 + im0[win_y][win_x]; //Sumatorio datos ventana imagen 0
-						vector0[i] = im0[win_y][win_x]; //Guardar datos
-						i++;
+						vector0[m] = im0[win_y][win_x]; //Guardar datos
+						m++;
 				}
 			}
 			average0 = count0 / pow(windowSize); //Media ventana imagen0
 			
-			for(i = 0; i < vector0.size; i++){//RECORRER OTRA VEZ VENTANA IMAGEN 0, esta vez usamos el vector
-				desTipica0 = desTipica0 + pow(vector0[i]-average0);
+			for(m = 0; m < vector0.size; m++){//RECORRER OTRA VEZ VENTANA IMAGEN 0, esta vez usamos el vector
+				desTipica0 = desTipica0 + pow(vector0[m]-average0);
 			}
 			desTipica0 = sqrt(desTipica0 / pow(windowSize));
 
 			for (int d = 0; d < disparity; d++){ //RECORRER VENTANAS IMAGEN 1
 
-				int j = 0;
+				int n = 0;
 				for (int win_y = j - windowSize / 2; win_y < windowSize; win_y++){ //CONTADOR DE LOS DATOS VENTANA IMAGEN 1
 					for (int win_x = i - windowSize / 2; win_x < windowSize; win_x++){
 						count1 = count1 + im1[win_y][win_x];
-						vector[j] = im1[win_y][win_x]; //CREAR VECTOR DE DATOS VENTANA IMAGEN 1
-						j++;
+						vector[n] = im1[win_y][win_x]; //CREAR VECTOR DE DATOS VENTANA IMAGEN 1
+						n++;
 					}
 				}
 				average1 = count1 / pow(windowSize);
 				
-				for(J = 0; J < vector0.size; J++){//RECORRER OTRA VEZ VENTANA IMAGEN 1, esta vez usamos el vector
-					desTipica1 = desTipica1 + pow(vector1[J]-average0);
+				for(n = 0; n < vector0.size; n++){//RECORRER OTRA VEZ VENTANA IMAGEN 1, esta vez usamos el vector
+					desTipica1 = desTipica1 + pow(vector1[n]-average0);
 				}
 				desTipica1 = sqrt(desTipica1 / pow(windowSize));
 
-				for (j = 0; j < vector0.size; j++) { //CALCULAR COVARIANZA CON LOS DATOS GUARDADOS EN LOS VECTORES
-					covarianza = covarianza + ((vector0[j] - average0) * (vector1[j] - average1));
+				for (n = 0; n < vector0.size; n++) { //CALCULAR COVARIANZA CON LOS DATOS GUARDADOS EN LOS VECTORES
+					covarianza = covarianza + ((vector0[n] - average0) * (vector1[n] - average1));
 				}
 				covarianza = covarianza / pow(windowSize);
 
