d7ac92fb7ea193dd57ad831c2c136ffb9b2d3794
fannygt95 <32515988+fannygt95@users.noreply.github.com>
2019-05-03 20:14:24 +0300

    Update main.c

--- main.c
+++ main.c
@@ -7,22 +7,21 @@
 #include <time.h>
 #include "lodepng.h"
 
+#define CL_USE_DEPRECATED_OPENCL_1_1_APIS
 
-#define CL_USE_DEPRECATED_OPENCL_1_2_APIS
-#include <Windows.h>
 #include <CL/cl.h>
+#include <Windows.h>
 
-using namespace std;
 
-const int resizear       = 4;    // valor por el que dividimos la imagen
-const uint32_t half_winx = 8;    
-const uint32_t half_winy = 15;   
-const int threshold      = 8;    // Threshold para calcular el mapa final
-const uint32_t win_area  = 527;  // (half_winx x 2 + 1) x (half_winY x 2 + 1) = win_area
+const int resizear = 4;    // valor por el que dividimos la imagen
+const uint32_t half_winx = 8;
+const uint32_t half_winy = 15;
+const int threshold = 8;    // Threshold para calcular el mapa final
+const uint32_t win_area = 527;  // (half_winx x 2 + 1) x (half_winY x 2 + 1) = win_area
 
-int maxDisp= 64;   
-int minDisp= 0;
-
+int maxDisp = 64;
+int minDisp = 0;
+/*
 typedef struct cl_image_desc {
 	cl_mem_object_type image_type;
 	size_t image_width;
@@ -35,16 +34,27 @@ typedef struct cl_image_desc {
 	cl_uint num_samples;
 	cl_mem buffer;
 } cl_image_desc;
-
+*/
 cl_image_desc imgDescriptor;        // Image descriptor contains type and dimensions of the image
 cl_image_format format = { CL_RGBA, CL_UNSIGNED_INT8 };
 
-char *leerFichero(const char *filename);
+
+char *leerFıchero(const char *filename);
 cl_kernel file_to_kernel(cl_context ctx, char const *kernel, char const *kernel_name);
-void normalizar(uint8_t* lastMap, uint32_t width, uint32_t height);
-uint8_t* SustituirCeros(const uint8_t* lastMap, uint32_t w, uint32_t h);
+void normalizar(uint8_t* dispMap, uint32_t w, uint32_t h);
+uint8_t* SustituirCeros(const uint8_t* dispMap, uint32_t w, uint32_t h);
+
 
 int32_t main(){
+
+	static LARGE_INTEGER frequencyStart, frequencyEnd; //variables para medir lo que tarda el programa en ejecutar
+	LARGE_INTEGER starTime, endTime;
+
+	if (frequencyStart.QuadPart == 0)
+		::QueryPerformanceFrequency(&frequencyStart);
+	::QueryPerformanceCounter(&starTime);
+
+	printf("Comenzando programa, esto tardará unos minutos...\n");
 
 	uint8_t *image0, *image1; // Imagenes iniciales
 	uint8_t *dDisparity, *Disparity;
@@ -54,32 +64,22 @@ int32_t main(){
 	uint32_t w0, h0, w1, h1;            // tamaños originales
 
 
-	static LARGE_INTEGER frequencyStart, frequencyEnd; //variables para medir lo que tarda el programa en ejecutar
-	LARGE_INTEGER starTime, endTime;
-	
-	//INICIALIZANDO PROGRAMA
-	if (frequencyStart.QuadPart == 0) 
-		::QueryPerformanceFrequency(&frequencyStart);
-	::QueryPerformanceCounter(&starTime);
-	
-	printf("Comenzando programa, esto tardará unos minutos...\n");
-
-
 	cl_context ctx;
 	cl_command_queue queue;
 	cl_int status;
 
-	const size_t localWorkSize[] = { 16, 16 };
-	const size_t globalWorkSize[] = { 504, 736 };
+	const size_t localWorkSize[] = { 4, 5 };       // Local work size
+	  //const size_t localWorkSize[]    = {2, 2};       // Local work size (Odroid)
+	const size_t globalWorkSize[] = { 504, 735 };   // Global work size
 
-	const size_t localWorkSize1D[] = { localWorkSize[0] * localWorkSize[1] };
-	const size_t globalWorkSize1D[] = { globalWorkSize[0] * globalWorkSize[1] };
+	const size_t localWorkSize1D[] = { localWorkSize[0] * localWorkSize[1] };      // 1-dimentional local work size
+	const size_t globalWorkSize1D[] = { globalWorkSize[0] * globalWorkSize[1] };    // 1-dimentional global work size
 
 
 	// CARGAR IMAGENES EN MEMORIA Y COMPROBAR POSIBLES ERRORES
 	lodepng_decode32_file(&image0, &w0, &h0, "im0.png");  //CARGANDO IMAGEN DERECHA
 	lodepng_decode32_file(&image1, &w1, &h1, "im1.png");  //CARGANDO IMAGEN IZQUIERDA
-
+	
 	if (w0 != w1 || h0 != h1) {
 		printf("Error, tamaños diferentes.\n");
 		free(image0);
@@ -89,18 +89,32 @@ int32_t main(){
 	new_width = w0 / resizear;
 	new_height = h0 / resizear;
 
-	// KERNEL
+
+
+	// ******** Setup OpenCL environment to run the kernel ********
 	cl_platform_id platform = 0;
 	cl_device_id device = 0;
 	cl_context_properties props[3] = { CL_CONTEXT_PLATFORM, 0, 0 };
 
 	status = clGetPlatformIDs(1, &platform, NULL);
+	printf("clGetPlatformIDs status == CL_SUCCESS - %d\n", status == CL_SUCCESS);
 	int gpu = 1; // O : CPU, 1 : GPU
 	status = clGetDeviceIDs(platform, gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU, 1, &device, NULL);
 
 	props[1] = (cl_context_properties)platform;
-	ctx = clCreateContext(props, 1, &device, NULL, NULL, &status);	//CREATE CONTEXT FOR OpenCL
-	queue = clCreateCommandQueue(ctx, device, 0, &status); 		//CREATE QUEUE FOR OpenCL CONTEXT
+	// context
+	ctx = clCreateContext(props, 1, &device, NULL, NULL, &status);
+	if (status != CL_SUCCESS) {
+		fprintf(stderr, "Fail to create context for OpenCL !\n");
+		abort();
+	}
+	// queue
+	queue = clCreateCommandQueue(ctx, device, 0, &status);
+	if (status != CL_SUCCESS) {
+		fprintf(stderr, "Fail to create queue for OpenCL context !\n");
+		abort();
+	}
+
 
 	// MEMORIA PARA IMAGENES Y DISPARITY MAPS
 	cl_mem clmemImage0 = clCreateBuffer(ctx, CL_MEM_READ_ONLY, new_width*new_height, 0, &status); //CREAR BUFFER IMAGEN DERECHA
@@ -111,11 +125,13 @@ int32_t main(){
 
 	cl_mem FinalMap = clCreateBuffer(ctx, CL_MEM_READ_ONLY, new_width*new_height, 0, &status); //CREAR BUFFER PARA FINAL DISPARITY MAP 
 
-	// LEER ARCHIVOS DE KERNEL E INICIALIZAR
-	char *resize_kernel_file = leerFichero("ReduceGrayMatrix.cl");
-	char *zncc_kernel_file = leerFichero("ZNCC.cl");
-	char *cross_check_kernel_file = leerFichero("CalculateLastMap.cl");
 
+	// ******** Read kernel file ********
+	char *resize_kernel_file = leerFıchero("resize.cl");
+	char *zncc_kernel_file = leerFıchero("zncc.cl");
+	char *cross_check_kernel_file = leerFıchero("cross_check.cl");
+
+	// ******* Init cl kernel from files *******
 	imgDescriptor.image_type = CL_MEM_OBJECT_IMAGE2D;
 	imgDescriptor.image_width = w0;
 	imgDescriptor.image_height = h0;
@@ -126,13 +142,14 @@ int32_t main(){
 	Disparity = (uint8_t*)malloc(new_width*new_height);
 
 
-	cl_kernel resize_kernel = file_to_kernel(ctx, resize_kernel_file, "ReduceGrayMatrix");
-	cl_kernel zncc_kernel = file_to_kernel(ctx, zncc_kernel_file, "ZNCC");
-	cl_kernel cross_check_kernel = file_to_kernel(ctx, cross_check_kernel_file, "CalculateLastMap");
+	cl_kernel resize_kernel = file_to_kernel(ctx, resize_kernel_file, "resize");
+	cl_kernel zncc_kernel = file_to_kernel(ctx, zncc_kernel_file, "zncc");
+	cl_kernel cross_check_kernel = file_to_kernel(ctx, cross_check_kernel_file, "cross_check");
 
 	// CREAR OBJETOS IMAGENES EN MEMROIA
 	cl_mem clmemOrigImage0 = clCreateImage2D(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &format, imgDescriptor.image_width, imgDescriptor.image_height, imgDescriptor.image_row_pitch, image0, &status);
 	cl_mem clmemOrigImage1 = clCreateImage2D(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, &format, imgDescriptor.image_width, imgDescriptor.image_height, imgDescriptor.image_row_pitch, image1, &status);
+
 
 	// LLAMADA A LOS KERNELS
 	status = 0;
@@ -148,6 +165,7 @@ int32_t main(){
 	clFinish(queue);
 	status = clEnqueueReadBuffer(queue, clmemImage0, CL_TRUE, 0, new_width*new_height, Disparity, 0, NULL, NULL);
 	status = clEnqueueReadBuffer(queue, clmemImage1, CL_TRUE, 0, new_width*new_height, Disparity, 0, NULL, NULL);
+
 
 	// MAPA DE DISPARIDAD DE IMAGEN 0 A IMAGEN 1
 	status = 0;
@@ -180,6 +198,7 @@ int32_t main(){
 
 	status = clEnqueueNDRangeKernel(queue, zncc_kernel, 2, NULL, (const size_t*)&globalWorkSize, (const size_t*)&localWorkSize, 0, NULL, NULL);
 
+
 	// Cross checking kernel
 	status = 0;
 	status = clSetKernelArg(cross_check_kernel, 0, sizeof(IzqDer), &IzqDer);
@@ -189,24 +208,30 @@ int32_t main(){
 
 	status = clEnqueueNDRangeKernel(queue, cross_check_kernel, 1, NULL, (const size_t*)&globalWorkSize1D, (const size_t*)&localWorkSize1D, 0, NULL, NULL);
 
+
 	clFinish(queue);
-	status = clEnqueueReadBuffer(queue, FinalMap, CL_TRUE, 0, new_width*new_height, dDisparity, 0, NULL, NULL);
+	/*status = clEnqueueReadBuffer(queue, clmemDispMapCrossCheck, CL_TRUE, 0, Width*Height, dDisparity, 0, NULL, NULL);
+	if (status != CL_SUCCESS) {
+		fprintf(stderr, "'cross_check_kernel': Failed to send the data to host !\n");
+		abort();
+	}
+	*/
 
 	// ******** run occlusion_filling & nomalize on host-code ********
 	Disparity = SustituirCeros(dDisparity, new_width, new_height);
 	normalizar(Disparity, new_width, new_height);
 
-	//ACABANDO PROGRAMA
+
 	if (frequencyEnd.QuadPart == 0)
 		::QueryPerformanceFrequency(&frequencyEnd);
 	::QueryPerformanceCounter(&endTime);
 
 
-	printf("Tiempo total: %f s.\n", (endTime.QuadPart / double(frequencyEnd.QuadPart)) - (starTime.QuadPart / double(frequencyStart.QuadPart));
+	printf("Tiempo total: %f s.\n", (endTime.QuadPart / double(frequencyEnd.QuadPart)) - (starTime.QuadPart / double(frequencyStart.QuadPart)));
 
 
-	// GUARDAR MAPA FINAL Y LIBERAR ESPACIO EN MEMORIA
-	lodepng_encode_file("depthmap.png", Disparity, new_width, new_height, LCT_GREY, 8);
+	// ******** Save file to working directory (setup working directory may differ from IDEs) ********
+	err = lodepng_encode_file("depthmap.png", Disparity, new_width, new_height, LCT_GREY, 8);
 	free(image0);
 	free(image1);
 	free(resize_kernel_file);
@@ -229,93 +254,124 @@ int32_t main(){
 	clReleaseMemObject(DerIzq);
 	clReleaseMemObject(FinalMap);
 
+	if (err) {
+		printf("Error when saving the final 'depthmap.png' %u: %s\n", err, lodepng_error_text(err));
+		return -1;
+	}
+	return 0;
 }
 
-char *leerFichero(const char *filename){ //LEER KERNEL
-
-	FILE *fich = fopen(filename, "r");
-	fseek(fich, 0, SEEK_END);
-	size_t size = ftell(fich);
-	fseek(fich, 0, SEEK_SET);
+/******************************************************************************
+ *  Function that use to read kernel file
+ */
+char *leerFıchero(const char *filename)
+{
+	FILE *f = fopen(filename, "r");
+	fseek(f, 0, SEEK_END);
+	size_t size = ftell(f);
+	fseek(f, 0, SEEK_SET);
 
 	char *res = (char *)malloc(size + 1);
-
-	if (fread(res, 1, size, fich) < size) { // check error
-		perror("Error de lectura");
+	if (!res) { // check error
+		perror("Fail to read file, can not allocation memory !");
 		abort();
 	}
 
-	fclose(fich);
+	// read file from stream
+	if (fread(res, 1, size, f) < size) { // check error
+		perror("Fail to read file, fread abort !");
+		abort();
+	}
+
+	fclose(f);
 	res[size] = '\0';
 	return res;
 }
 
 /******************************************************************************
-*  Function that use to build kernel from file
-*/
-cl_kernel file_to_kernel(cl_context ctx, char const *kernel, char const *kernel_name){ //CONSTRUIR KERNEL
+ *  Function that use to build kernel from file
+ */
+cl_kernel file_to_kernel(cl_context ctx, char const *kernel, char const *kernel_name)
+{
 	cl_int status;
 
+	printf("Building kernel '%s'\n", kernel_name);
+	// get kernel size
 	size_t sizes[] = { strlen(kernel) };
 
+	// create program
 	cl_program program = clCreateProgramWithSource(ctx, 1, &kernel, sizes, &status);
+	if (status != CL_SUCCESS) {
+		fprintf(stderr, "Fail to create program, in kernel file: '%s' !\n", kernel_name);
+		abort();
+	}
 
+	// build program
 	status = clBuildProgram(program, 0, NULL, NULL, NULL, NULL);
+	if (status != CL_SUCCESS) {
+		fprintf(stderr, "Fail to build program, in kernel file: '%s' !\n", kernel_name);
+		abort();
+	}
 
+	// create our kernel from program
 	cl_kernel res = clCreateKernel(program, kernel_name, &status);
-
+	if (status != CL_SUCCESS) {
+		fprintf(stderr, "Fail to create kernel, in kernel file: '%s' !\n", kernel_name);
+		abort();
+	}
 	clReleaseProgram(program);
+	printf("Succefully builed kernel '%s'\n", kernel_name);
 	return res;
 }
 
 /******************************************************************************
-*  Replace each pixel with zero value with the nearest non-zero pixel value
-*/
-uint8_t* SustituirCeros(const uint8_t* dispMap, uint32_t w, uint32_t h){
-	int32_t i, j;
-	int32_t auxValor;
-	int32_t win_y, win_x;
-	bool aux;
+ *  Replace each pixel with zero value with the nearest non-zero pixel value
+ */
+uint8_t* SustituirCeros(const uint8_t* dispMap, uint32_t w, uint32_t h) {
+	int32_t i, j, ii, jj, k;
+	bool flag; // flag for nearest non-zero pixel value
 
-	uint8_t* res = (uint8_t*)malloc(w*h);
+	uint8_t* result = (uint8_t*)malloc(w*h);
 
 	for (i = 0; i < h; i++) {
 		for (j = 0; j < w; j++) {
-			res[i*w + j] = dispMap[i*w + j];
+			// If the value of the pixel is zero, perform the occlusion filling by nearest non-zero pixel value
+			result[i*w + j] = dispMap[i*w + j];
 			if (dispMap[i*w + j] == 0) {
-				aux = true;
-				auxValor = 0;
-				while (aux) {
-					auxValor++;
-					win_x = -auxValor;
-					for (win_y = -auxValor; win_y <= auxValor && aux; win_y++) {
-						if (0 <= i + win_y && i + win_y < h && 0 <= j + win_x && j + win_x < w && dispMap[(i + win_y)*w + (j + win_x)] != 0) {
-							res[i*w + j] = dispMap[(i + win_y)*w + (j + win_x)];
-							aux = false;
+				// Search of non-zero pixel in the neighborhood i,j, neighborhoodsize++
+				flag = true;
+				k = 0;
+				while (flag) {
+					k++;
+					jj = -k;
+					for (ii = -k; ii <= k && flag; ii++) {
+						if (0 <= i + ii && i + ii < h && 0 <= j + jj && j + jj < w && dispMap[(i + ii)*w + (j + jj)] != 0) {
+							result[i*w + j] = dispMap[(i + ii)*w + (j + jj)];
+							flag = false;
 							break;
 						}
 					}
-					win_x = auxValor;
-					for (win_y = -auxValor; win_y <= auxValor && aux; win_y++) {
-						if (0 <= i + win_y && i + win_y < h && 0 <= j + win_x && j + win_x < w && dispMap[(i + win_y)*w + (j + win_x)] != 0) {
-							res[i*w + j] = dispMap[(i + win_y)*w + (j + win_x)];
-							aux = false;
+					jj = k;
+					for (ii = -k; ii <= k && flag; ii++) {
+						if (0 <= i + ii && i + ii < h && 0 <= j + jj && j + jj < w && dispMap[(i + ii)*w + (j + jj)] != 0) {
+							result[i*w + j] = dispMap[(i + ii)*w + (j + jj)];
+							flag = false;
 							break;
 						}
 					}
-					win_y = -auxValor;
-					for (win_x = -auxValor + 1; win_x <= auxValor - 1 && aux; win_x++) {
-						if (0 <= i + win_y && i + win_y < h && 0 <= j + win_x && j + win_x < w && dispMap[(i + win_y)*w + (j + win_x)] != 0) {
-							res[i*w + j] = dispMap[(i + win_y)*w + (j + win_x)];
-							aux = false;
+					ii = -k;
+					for (jj = -k + 1; jj <= k - 1 && flag; jj++) {
+						if (0 <= i + ii && i + ii < h && 0 <= j + jj && j + jj < w && dispMap[(i + ii)*w + (j + jj)] != 0) {
+							result[i*w + j] = dispMap[(i + ii)*w + (j + jj)];
+							flag = false;
 							break;
 						}
 					}
-					win_y = auxValor;
-					for (win_x = -auxValor + 1; win_x <= auxValor && aux; win_x++) {
-						if (0 <= i + win_y && i + win_y < h && 0 <= j + win_x && j + win_x < w && dispMap[(i + win_y)*w + (j + win_x)] != 0) {
-							res[i*w + j] = dispMap[(i + win_y)*w + (j + win_x)];
-							aux = false;
+					ii = k;
+					for (jj = -k + 1; jj <= k && flag; jj++) {
+						if (0 <= i + ii && i + ii < h && 0 <= j + jj && j + jj < w && dispMap[(i + ii)*w + (j + jj)] != 0) {
+							result[i*w + j] = dispMap[(i + ii)*w + (j + jj)];
+							flag = false;
 							break;
 						}
 					}
@@ -323,31 +379,27 @@ uint8_t* SustituirCeros(const uint8_t* dispMap, uint32_t w, uint32_t h){
 			}
 		}
 	}
-	return res;
+	return result;
 }
 
 /******************************************************************************
-*  Normalize the final disparity map
-*/
-void normalizar(uint8_t* lastMap, uint32_t width, uint32_t height) {
-	uint8_t maxValue = 0;
-	uint8_t minValue = UCHAR_MAX;
-	uint32_t i;
-	for (i = 0; i < width*height; i++) {
-		if (lastMap[i]>maxValue){
-			maxValue = lastMap[i];
-		}
+ *  Normalize the final disparity map */
 
-		if (lastMap[i]<minValue){
-			minValue = lastMap[i];
-		}
+void normalizar(uint8_t* dispMap, uint32_t w, uint32_t h) {
+	uint8_t maxValue = 0, minValue = UCHAR_MAX;
+	uint32_t i;
+	for (i = 0; i < w*h; i++) {
+		if (dispMap[i] > maxValue) { maxValue = dispMap[i]; }
+		if (dispMap[i] < minValue) { minValue = dispMap[i]; }
 	}
+	// Nomarlize to grey scale 0..255(UCHAR_MAX)
 	maxValue -= minValue;
-	for (i = 0; i < width*height; i++) {
-		lastMap[i] = (UCHAR_MAX*(lastMap[i] - minValue) / maxValue);
+	for (i = 0; i < w*h; i++) {
+		if(maxValue != 0) {
+			dispMap[i] = (UCHAR_MAX*(dispMap[i] - minValue) / maxValue);
+		}
+		else {
+			dispMap[i] = 15;
+		}	
 	}
 }
-
-
-
-
